d4739c9fcdf9382e4c6fc922d7e8308d7b88926f
ktk <akashsiddamshetty@gmail.com>
2022-05-20 22:51:55 +0530

    responsive carousels

--- src/components/homepage/allproducts/Carouselfeatured.js
+++ src/components/homepage/allproducts/Carouselfeatured.js
@@ -51,7 +51,25 @@ const Carouselfeatured = ({ products, addToCart }) => {
       <Swiper
         autoplay={true}
         // loop={true}
-        navigation={true} slidesPerView={4} spaceBetween={10} modules={[Navigation]} className="mySwiper">
+        navigation={true}
+        slidesPerView={1}
+        spaceBetween={10}
+        modules={[Navigation]}
+        breakpoints={{
+          640: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+          },
+          768: {
+            slidesPerView: 3,
+            spaceBetween: 20,
+          },
+          1024: {
+            slidesPerView: 4,
+            spaceBetween: 20,
+          },
+        }}
+        className="mySwiper">
         {Product}
       </Swiper>
     </>

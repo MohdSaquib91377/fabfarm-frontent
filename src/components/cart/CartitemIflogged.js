820726c226100ff8c0ea3641414f32e5af74073b
ktk <akashsiddamshetty@gmail.com>
2022-06-04 15:54:59 +0530

    iflogged in on decrease delete API

--- src/components/cart/CartitemIflogged.js
+++ src/components/cart/CartitemIflogged.js
@@ -4,15 +4,17 @@ import { faIndianRupee, faMinus, faPlus, faTrash } from '@fortawesome/free-solid
 import { removeFromCart, incrementQuantity, decrementQuantity } from '../../redux/actions/productActions';
 import { connect } from 'react-redux';
 import useAxiosPrivate from '../../hooks/useAxiosPrivate';
-const CartitemIflogged = ({ items, user, isAuthorized, removeFromCart, incrementQuantity, decrementQuantity }) => {
+const CartitemIflogged = ({ items, isAuthorized, removeFromCart, incrementQuantity, decrementQuantity }) => {
     // const [items, setItems] = useState([])
     // const { id, image: [{ image }], name, price, quantity } = items;
+    const [decrease, setDecrease] = useState("")
+    const [loader, setloader] = useState(false);
     const axiosPrivate = useAxiosPrivate();
     const deleteCartItems = (id) => {
         removeFromCart(id);
         if (isAuthorized) {
             axiosPrivate.delete('/api/v1/cart/add-to-cart/', {
-                 data: {
+                data: {
                     product_id: id
                 }
             })
@@ -24,27 +26,39 @@ const CartitemIflogged = ({ items, user, isAuthorized, removeFromCart, increment
                 })
         }
     }
-    // useEffect(() => {
-    //     if (isAuthorized) {
-    //         axios.get('/api/v1/cart/add-to-cart/', {
-    //             headers: {
-    //                 Authorization: `Bearer ${user.access}`
-    //             }
-    //         })
-    //             .then(response => {
-    //                 setItems(response.data)
-    //             })
-    //             .catch(response => {
-    //                 console.log(response)
-    //             })
-    //     }
-    // }, [isAuthorized, user])
+    const decreaseCount = (id) => {
+        setDecrease(id)
+        decrementQuantity(id)
+    }
+    useEffect(() => {
+        items.map((data) => {
+            if (Object.keys(data).some(key => key === 'cartQuantity')) {
+                if (data.product.id == decrease && data.cartQuantity < 2 ) {
+                    console.log(data.cartQuantity)
+                    setloader(true)
+                    axiosPrivate.delete('/api/v1/cart/add-to-cart/', {
+                        data: {
+                            product_id: decrease
+                        }
+                    })
+                        .then(response => {
+                            setloader(false)
+                            console.log(response)
+                        })
+                        .catch(response => {
+                            setloader(false)
+                            console.log(response)
+                        })
+                }
+            }
+        })
+    }, [decrease])
     const cartItems = items.map((data, i) => {
         if (Object.keys(data).some(key => key === 'cartQuantity')) {
             const { cartQuantity, product: { id, image: [{ image }], name, price, quantity } } = data;
 
             return (
-                <div 
+                <div
                     key={i}
                     style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-around', alignItems: 'center', margin: "15px 0" }}>
                     <div className="cart_block">
@@ -55,15 +69,20 @@ const CartitemIflogged = ({ items, user, isAuthorized, removeFromCart, increment
                     </div>
                     <div className="cart_block">
                         <h5>{name}</h5>
-                        <div className="item_quantity" style={{ display: 'flex', justifyContent: 'space-between' }}>
-                            <button className="quantity_minus" onClick={() => decrementQuantity(id)} ><FontAwesomeIcon icon={faMinus} /></button>
-                            <input
-                                type="text"
-                                value={cartQuantity}
-                                className="quantity"
-                                disabled />
-                            <button className="quantity_plus" onClick={() => incrementQuantity(id)} ><FontAwesomeIcon icon={faPlus} /></button>
-                        </div>
+                        {
+                            loader ?
+                                <div>Loading...</div>
+                                :
+                                <div className="item_quantity" style={{ display: 'flex', justifyContent: 'space-between' }}>
+                                    <button className="quantity_minus" onClick={() => decreaseCount(id)} ><FontAwesomeIcon icon={faMinus} /></button>
+                                    <input
+                                        type="text"
+                                        value={cartQuantity}
+                                        className="quantity"
+                                        disabled />
+                                    <button className="quantity_plus" onClick={() => incrementQuantity(id)} ><FontAwesomeIcon icon={faPlus} /></button>
+                                </div>
+                        }
                     </div>
                     <div className="cart_block">
                         <h4 style={{ display: 'flex' }}><span><FontAwesomeIcon icon={faIndianRupee} /></span>{cartQuantity * price}</h4>

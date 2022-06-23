4fa4973006cfe50f3f75f786c3f80bb86e292056
ktk <akashsiddamshetty@gmail.com>
2022-06-23 12:07:02 +0530

    counter scroll position updated dynamic

--- src/components/homepage/Counter.js
+++ src/components/homepage/Counter.js
@@ -1,15 +1,28 @@
 import React from 'react'
+import { useRef, useState, useEffect } from "react";
 import Countup from '../countup/Countup'
 
 const Counter = () => {
+    const boxRef = useRef();
+    const [y, setY] = useState();
+    const getPosition = () => {
+        const y = boxRef.current.offsetTop;
+        setY(y);
+    }
+    useEffect(() => {
+        getPosition();
+    }, []);
+    useEffect(() => {
+        window.addEventListener("resize", getPosition);
+    }, []);
     return (
-        <div className="clv_counter_wrapper clv_section">
+        <div className="clv_counter_wrapper clv_section" ref={boxRef}>
             <div className="container">
                 <div className="row justify-content-center">
                     <div className="col-lg-8 col-md-8">
                         <div className="clv_heading white_heading">
                             <h3>We Are The Experts Of This Field.</h3>
-                            <div className="clv_underline"><img src="images/underline2.png" alt="image" /></div>                            
+                            <div className="clv_underline"><img src="images/underline2.png" alt="image" /></div>
                         </div>
                     </div>
                 </div>
@@ -22,7 +35,7 @@ const Counter = () => {
                                 </div>
                                 <div className="counter_text">
                                     <h4><span className="count_no" data-to="26" data-speed="3000">
-                                        <Countup start={0} end={26} timer={50} scrollStart={3000} />
+                                        <Countup start={0} end={26} timer={50} scrollStart={y - 200} />
                                     </span><span>k+</span></h4>
                                     <h5>happy customers</h5>
                                 </div>
@@ -35,7 +48,7 @@ const Counter = () => {
                                 </div>
                                 <div className="counter_text">
                                     <h4><span className="count_no" data-to="700" data-speed="3000">
-                                        <Countup start={0} end={700} timer={50} scrollStart={3000} />
+                                        <Countup start={0} end={700} timer={50} scrollStart={y - 200} />
                                     </span><span>+</span></h4>
                                     <h5>Seeds Types</h5>
                                 </div>
@@ -48,7 +61,7 @@ const Counter = () => {
                                 </div>
                                 <div className="counter_text">
                                     <h4><span className="count_no" data-to="200" data-speed="3000">
-                                        <Countup start={0} end={200} timer={50} scrollStart={3000} />
+                                        <Countup start={0} end={200} timer={50} scrollStart={y - 200} />
                                     </span><span>+</span></h4>
                                     <h5>Soil Types</h5>
                                 </div>
@@ -61,7 +74,7 @@ const Counter = () => {
                                 </div>
                                 <div className="counter_text">
                                     <h4><span className="count_no" data-to="6" data-speed="3000">
-                                        <Countup start={0} end={6} timer={100} scrollStart={3000} />
+                                        <Countup start={0} end={6} timer={100} scrollStart={y - 200} />
                                     </span><span>k+</span></h4>
                                     <h5>award winner</h5>
                                 </div>

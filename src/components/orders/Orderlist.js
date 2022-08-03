987e1146090512301020924f205a03e62d41e537
ktk <akashsiddamshetty@gmail.com>
2022-08-03 17:12:39 +0530

    -- issue #117 fixed

--- src/components/orders/Orderlist.js
+++ src/components/orders/Orderlist.js
@@ -1,3 +1,5 @@
+import { faIndianRupee } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React, { useEffect, useRef, useState } from 'react'
 import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
@@ -79,7 +81,7 @@ const Orderlist = ({ user }) => {
                 </div>
                 <Link to={`/orderproductdetails/${order}`}><h6>{name}</h6></Link>
                 <h6>Order Id : {order}</h6>
-                <h6>Price: {price}</h6>
+                <h6>Price: <FontAwesomeIcon icon={faIndianRupee} /> {price}</h6>
                 <h6>Quantity: {quantity}</h6>
                 <h6>Status: {status}</h6>
                 {

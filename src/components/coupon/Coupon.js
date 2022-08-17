83286ab31a805bdf560a96d81a2c7c4be656967f
ktk <akashsiddamshetty@gmail.com>
2022-08-17 13:40:06 +0530

    --issue #200 fixed

--- src/components/coupon/Coupon.js
+++ src/components/coupon/Coupon.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { connect } from 'react-redux';
 import useAxiosPrivate from '../../hooks/useAxiosPrivate'
 import { setCouponDetails } from '../../redux/actions/productActions';
@@ -9,32 +9,73 @@ const Coupon = ({ setCouponDetails }) => {
         couponcode: ""
     };
     const [coupon, setCoupon] = useState({ initialValues })
+    const [couponErrors, setCouponErrors] = useState({})
+    const [isSubmit, setIsSubmit] = useState(false)
     const handleChange = (e) => {
         const { name, value } = e.target;
         setCoupon({ coupon, [name]: value })
     }
-    const applyCoupon = async () => {
-        try {
-            const response = await axiosPrivate.get(`/api/v1/coupon/apply/${coupon.couponcode}/`)
-            setCouponDetails({
-                couponDetails: response.data.data,
-                couponName: coupon.couponcode
-            })
-        } catch (error) {
-            throw error
-        }
+    const handleApply = () => {
+        setCouponErrors(validateCoupon(coupon))
+        setIsSubmit(true)
     }
+    const validateCoupon = (values) => {
+        const errors = {};
+        if (!values.couponcode) {
+            errors.couponcode = 'Coupon Code is requied!'
+        }
+        return errors;
+    }
+    useEffect(() => {
+        let isMounted = true;
+        const controller = new AbortController();
+        const applyCoupon = async () => {
+            setIsSubmit(false)
+            try {
+                const response = await axiosPrivate.get(`/api/v1/coupon/apply/${coupon.couponcode}/`)
+                setCouponDetails({
+                    couponDetails: response.data.data,
+                    couponName: coupon.couponcode
+                })
+            } catch (error) {
+                setCouponErrors({ couponcode: error?.response?.data?.message })
+                throw error
+            }
+        }
+        if (Object.keys(couponErrors).length === 0 && isMounted && isSubmit) {
+            applyCoupon();
+        }
+        return () => {
+            isMounted = false;
+            controller.abort();
+        }
+    })
+
     return (
-        <>
-            <input
-                name='couponcode'
-                placeholder='Coupon code'
-                value={coupon.couponcode}
-                className="coupenCodeInput"
-                onChange={handleChange}
-                type="text" />
-            <button  onClick={() => applyCoupon()} className="buttonViewMore">Apply</button>
-        </>
+        <div
+            style={{
+                display: 'flex',
+                justifyContent: 'space-between',
+                width: '100%'
+            }}>
+            <div>
+                <input
+                    name='couponcode'
+                    placeholder='Coupon code'
+                    value={coupon.couponcode}
+                    className="coupenCodeInput"
+                    onChange={handleChange}
+                    type="text" />
+                <p style={{
+                    color:'red'
+                }}>{couponErrors.couponcode}</p>
+            </div>
+            <div>
+                <button
+                    onClick={() => handleApply()}
+                    className="buttonViewMore">Apply</button>
+            </div>
+        </div>
     )
 }
 const mapDispatchToProps = (dispatch) => {
